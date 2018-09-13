e8236b66277486f5265871c4fc5ba239808a3b55
Adam Fletcher <adamgingernut@googlemail.com>
2018-09-13 22:12:15 +0100

    Adding PyCharm Repo

---
+++ PycharmProjects/Introduction to Python/Strings/String length/len_function.py
@@ -0,0 +1,11 @@
+phrase = """
+It is a really long string
+triple-quoted strings are used
+to define multi-line strings
+"""
+
+first_half_len = int(len(phrase) / 2)
+print(first_half_len)
+
+first_half = phrase[:first_half_len]
+print(first_half)
